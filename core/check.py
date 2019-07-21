2f4964aca2718bd8af20e5e21113f63a2338b3dc
melis_m <matteo.melis@epitech.eu>
2019-07-21 15:39:45 +0200

    Add check on pkg kind, and a bit more logs to other things

--- core/check.py
+++ core/check.py
@@ -1,7 +1,9 @@
 import core.checks.executable as exe
 import core.checks.syntax_check as stx
+import core.checks.nature as nature
 
 
 def check_package(pkg):
     exe.ExecCheck().run()
     stx.DescriptionCheck(pkg).run()
+    nature.NatureCheck(pkg).run()
---
+++ core/checks/nature.py
@@ -0,0 +1,51 @@
+import os
+import core
+import core.log as log
+import core.checks.base as base
+import core.checks.utils as utils
+
+
+class NatureCheck(base.CheckWithManifest):
+    def __init__(self, pkg):
+        super().__init__(pkg, [None])
+        log.s("Checking package kind")
+
+    def validate(self, _):
+        length = len(os.listdir(self.pkg.cache))
+        return (self.pkg.is_effective and length != 1) \
+            or (not self.pkg.is_effective and length == 1)
+
+    def show(self, _):
+        if self.pkg.is_effective:
+            log.e("Package is effective but is missing a data.tar.gz")
+        else:
+            log.e("Package is virtual but has a data.tar.gz")
+
+    def diff(self, _):
+        target = 'virtual' if self.pkg.is_effective else 'effective'
+        log.i(f"Package kind would be changed to {target}")
+
+    def fix(self, _):
+        target = 'virtual' if self.pkg.is_effective else 'effective'
+        self.pkg.manifest['kind'] = target
+        self.pkg.is_effective = not self.pkg.is_effective
+
+    def edit(self, _):
+        if self.pkg.is_effective:
+            ans = log.q("Would you like to edit the manifest.toml (1) "
+                        "or to add files to the package (2)?")
+            if ans == "1":
+                super().edit(_)
+            elif ans == "2":
+                utils.open_shell(self.pkg.cache)
+            else:
+                log.w("Only recognized answers are 1 and 2")
+        else:
+            ans = log.q("Would you like to edit the manifest.toml (1) "
+                        "or to remove the files from the package? (2)")
+            if ans == "1":
+                super().edit(_)
+            elif ans == "2":
+                pass  # The rewrap at the end will ignore the files if the kind is virtual
+            else:
+                log.w("Only recognized answers are 1 and 2")
