7a10b06a79fcf74b44e5016b7cfca9364c02d6b5
melis_m <matteo.melis@epitech.eu>
2019-07-23 18:14:09 +0200

    Add check on package version semver compliance

--- core/check.py
+++ core/check.py
@@ -1,7 +1,9 @@
 import core.checks.executable as exe
 import core.checks.syntax_check as stx
+import core.checks.version_validity as version
 
 
 def check_package(pkg):
     exe.ExecCheck().run()
     stx.DescriptionCheck(pkg).run()
+    version.VersionValidityCheck(pkg).run()
---
+++ core/checks/version_validity.py
@@ -0,0 +1,30 @@
+import core.log as log
+import core.checks.base as base
+import semver
+
+
+class VersionValidityCheck(base.CheckWithManifest):
+    def __init__(self, pkg):
+        super().__init__(pkg, None)  # items is provided on the next line
+        self.items = [pkg.manifest['version']]
+
+    def validate(self, version):
+        try:
+            semver.valid(version, loose=False)
+            return True
+        except AttributeError:
+            return False
+
+    def show(self, version):
+        log.e(f"The version isn't semver compliant")
+
+    def diff(self, version):
+        log.w("No fix can be done automatically")
+        return False
+
+    def fix(self, version):
+        log.w("No fix can be done automatically")
+
+    def run(self):
+        log.s("Checking package version")
+        super().run()
